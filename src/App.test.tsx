fbf79a85e0972944f01511924801692d4df8fd8d
Adam Schønemann <adamschoenemann@gmail.com>
2022-03-17 20:18:30 +0100

    add test that warns

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { renderHook, WrapperComponent } from "@testing-library/react-hooks";
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+async function loadData() {}
+const MyContext = React.createContext(undefined as unknown as any);
+
+const MyProvider: React.FC = (props) => {
+  const [state, setState] = React.useState({ foo: 0 });
+  React.useEffect(() => {
+    async function load() {
+      await loadData();
+      setState({ foo: 20 });
+    }
+    load();
+  }, []);
+  return (
+    <MyContext.Provider value={state}>{props.children}</MyContext.Provider>
+  );
+};
+
+function useExampleHook() {
+  const state = React.useContext(MyContext);
+  return { state };
+}
+
+describe("minimal example", () => {
+  test("the example", async () => {
+    const wrapper: WrapperComponent<{}> = ({ children }) => (
+      <MyProvider>{children}</MyProvider>
+    );
+    const { result } = renderHook(() => useExampleHook(), {
+      wrapper,
+    });
+    expect(result.current.state).toEqual({ foo: 0 });
+  });
 });
